492e3b671b9c16f972fa625d9b1a3faacc5bc60a
EkaterinaSyrova <esyrova01@gmail.com>
2023-08-17 21:56:12 +1000

    -

---
+++ src/components/Counter.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="root">
+    <p>
+    </p>
+    <button
+        type="button"
+        @click="decrementCount"
+    >
+      Уменьшить
+    </button>
+    <input
+        type="number"
+        :value="count"
+        :min="minCount"
+    >
+    <button
+        type="button"
+        @click="incrementCount"
+    >
+      Увеличить
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyComponent',
+  data() {
+    return {
+      count: 1,
+      minCount: 0
+    }
+  },
+  methods: {
+    incrementCount() {
+      this.count++
+    },
+    decrementCount() {
+      if (this.count > this.minCount) {
+        this.count--
+      }
+    }
+  }
+}
+</script>
